8564f4ccd4221de5ae26a83126223e51ff8e8909
adamish717 <adam@piechnik.ca>
2017-08-02 21:06:30 -0700

    Countdown WIP

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -3,14 +3,10 @@ console.log('Welcome to Pipers Pub!');
 // require('jquery2');
 
 require('vendor/bootstrap.min');
-
 require('vendor/jquery.easing.1.3');
 require('vendor/jquery.flexslider-min');
 // require('vendor/jquery.stellar.min');
 require('vendor/jquery.waypoints.min');
-
 require('vendor/jquery.appear');
-
-require('vendor/CountDownTimer');
 
 require('custom.min');
