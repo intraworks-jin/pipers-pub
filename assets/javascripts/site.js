0317c92ba39b1220cc2d975534514b51491bfaa3
adamish717 <adam@piechnik.ca>
2017-08-03 14:05:24 -0700

    Use Helpers where possible + Cleanup

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -5,7 +5,6 @@ console.log('Welcome to Pipers Pub!');
 require('vendor/bootstrap.min');
 require('vendor/jquery.easing.1.3');
 require('vendor/jquery.flexslider-min');
-// require('vendor/jquery.stellar.min');
 require('vendor/jquery.waypoints.min');
 require('vendor/jquery.appear');
 
