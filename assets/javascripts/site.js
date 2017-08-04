89846971e36d599b88ca2bf6031c9fb3352fc805
adamish717 <adam@piechnik.ca>
2017-08-03 17:12:45 -0700

    Mom's Spaghetti, I'm cleaning out my closet

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -1,11 +1,9 @@
 console.log('Welcome to Pipers Pub!');
 
-// require('jquery2');
-
 require('vendor/bootstrap.min');
-require('vendor/jquery.easing.1.3');
+// require('vendor/jquery.easing.1.3');
 require('vendor/jquery.flexslider-min');
 require('vendor/jquery.waypoints.min');
-require('vendor/jquery.appear');
+// require('vendor/jquery.appear');
 
 require('custom.min');
