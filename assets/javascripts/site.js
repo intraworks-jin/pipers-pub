897c2ff4c07f61463396462cb2a364283c1f37ce
adamish717 <adam@piechnik.ca>
2017-08-09 01:02:58 -0700

    Add Easing back in

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Pipers Pub!');
 
 require('vendor/bootstrap.min');
-// require('vendor/jquery.easing.1.3');
+require('vendor/jquery.easing.1.3');
 require('vendor/jquery.flexslider-min');
 require('vendor/jquery.waypoints.min');
 // require('vendor/jquery.appear');
