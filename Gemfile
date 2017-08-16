63437ba14c8559431d139ff64608cf9a33fcccea
adamish717 <adam@piechnik.ca>
2017-08-16 11:11:42 -0700

    Add deploy to S3

--- Gemfile
+++ Gemfile
@@ -4,5 +4,8 @@ gem 'middleman-livereload'
 gem 'middleman', '~> 4.2'
 gem 'middleman-autoprefixer', '~> 2.7'
 gem 'middleman-aria_current'
+gem 'middleman-s3_sync'
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
+
+gem 'mime-types'
--- config.rb
+++ config.rb
@@ -27,3 +27,18 @@ activate :aria_current
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
 set :images_dir, 'img'
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'www.pipers-pub.ca'
+  s3_sync.region                     = 'us-west-2'
+  s3_sync.aws_access_key_id          = 'REDACTED: Use .s3_sync file with info in it'
+  s3_sync.aws_secret_access_key      = 'REDACTED: Use .s3_sync file with info in it'
+  s3_sync.delete                     = true
+  s3_sync.prefer_gzip                = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.prefix                     = ''
+  s3_sync.version_bucket             = false
+end
+
+require 'mime-types'
